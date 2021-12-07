d558e162777ae4210a920a9de16ddfdd6d91e922
Amaan-09 <79093439+Amaan-09@users.noreply.github.com>
2021-12-07 18:48:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-a="";
+video = "";
+status = "";
+function preload()
+{
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(300,250);
+    canvas.center();
+}
+
+function start()
+{
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects"
+
+}
+
+function modelLoaded()
+{
+console.log("Model Loaded!");
+status = true;
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 250);
+}
